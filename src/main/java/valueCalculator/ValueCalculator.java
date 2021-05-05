a37b813d45d8bdfc03b7deceb6ffee1a43c1930c
sivtcev <sivtcev@gmail.com>
2021-05-06 02:45:45 +0300

    fix second task

--- src/main/java/valueCalculator/ValueCalculator.java
+++ src/main/java/valueCalculator/ValueCalculator.java
@@ -8,9 +8,14 @@ import java.util.function.Function;
 
 public class ValueCalculator<K, V> {
 
-        public Future<V> compute(final K k, Function<K, V> f) {
-            Map<K, V> cash = new ConcurrentHashMap<>();
-            return CompletableFuture.supplyAsync(() -> cash.computeIfAbsent(k, f));
-        }
+    private Map<K, V> cash;
+
+    public ValueCalculator() {
+        this.cash = new ConcurrentHashMap<>();
+    }
+
+    public Future<V> compute(final K k, Function<K, V> f) {
+        return CompletableFuture.supplyAsync(() -> cash.computeIfAbsent(k, f));
+    }
 }
 
